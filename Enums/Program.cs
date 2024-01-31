e21167728d5706d9c1e0642acd3302d29eac042f
jayvirPMC <jayvirsinh.chhasatiya@pmcretail.com>
2024-01-31 10:48:21 +0530

    asp.net web app

---
+++ Enums/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Program {
+    public static void Main(string[] arg) {
+        // Console.WriteLine("jay mataji");
+        // Console.WriteLine(Days.mon); // mon
+        // Console.WriteLine((int)Days.mon); // 100
+        // Console.WriteLine(typeof(Days)); 
+
+        // foreach(Days d in Enum.GetValues(typeof(Days))) {
+        //     Console.WriteLine(d);
+        // }
+
+        // for(int i = 0; i <= 100; i++) {
+        //     if((i % 3 == 0) && (i % 5 == 0)) 
+        //     {
+        //         Console.WriteLine(i + " - " + "FizzBuzz"); 
+        //     } else if(i % 3 == 0)
+        //     {
+        //         Console.WriteLine(i + " - " + "Fizz");
+        //     } else if(i % 5 == 0)
+        //     {
+        //         Console.WriteLine(i + " - " + "Buzz");
+        //     } else {
+        //         Console.WriteLine(i);
+        //     }
+        // }
+
+        int hero = 10;
+        int monster = 10;
+
+        int monD = 0;
+        int heroD = 0;
+
+        do {
+            Random rnd = new Random();
+            monD = rnd.Next(1, 11);
+            monster = monster - monD;
+            Console.WriteLine("Monster was damaged and lost "+ monD +" health and now has "+monster+" health.");
+        } while(hero <= 0 || monster <= 0)
+    }
+}
+
+// enum Days {
+//     mon = 100,
+//     tue = 200,
+//     wed = 300,
+//     thu = 400,
+//     fri = 500,
+//     sat = 600,
+//     sun = 700
+// }
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program {
+    public static void Main(string[] args) {
+        int[] num = {12, 21, 32, 33, 23};
+        int[] den = {12, 21, 32, 0, 23, 32};
+        int[] ans = new int[num.Length];
+
+
+            for(int i = 0; i < num.Length; i++) {
+                try{
+                    ans[i] = num[i] / den[i + 1];
+                } catch(DivideByZeroException e) {
+                    Console.WriteLine("message: " + e.Message);
+                } catch(IndexOutOfRangeException ie) {
+                    Console.WriteLine("message: " + ie.Message);
+                }
+            }
+
+        foreach(int i in ans) {
+            Console.WriteLine(i);
+        }
+
+
+    }
+}
